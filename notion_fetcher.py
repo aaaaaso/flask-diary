aa9046d4dfa96f314d35c07d13172135cc1aed48
aaaaaso <aaaaasoaaa@gmail.com>
2025-07-06 21:44:51 +0900

    ok

--- notion_fetcher.py
+++ notion_fetcher.py
@@ -1,5 +1,6 @@
 import os
 from notion_client import Client
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,25 +13,36 @@ client = Client(auth=NOTION_TOKEN)
 def fetch_diary_entries():
     blocks = client.blocks.children.list(NOTION_PAGE_ID)["results"]
     diary = []
-    current_entry = None
+    current_date = None
+    current_lines = []
 
     for block in blocks:
         if block["type"] == "heading_2":
-            # 直前の日記を格納
-            if current_entry:
-                diary.append(current_entry)
-            current_entry = {
-                "date": block["heading_2"]["rich_text"][0]["plain_text"],
-                "html": ""
-            }
-        elif block["type"] == "paragraph" and current_entry:
+            if current_date and current_lines:
+                html = "\n".join([f"<p class='diary-block'>{line}</p>" for line in current_lines])
+                diary.append({"date": current_date, "html": html})
+                current_lines = []
+
+            # タイトルから日付取得
+            date_text = block["heading_2"]["rich_text"][0]["plain_text"]
+            try:
+                date_obj = datetime.strptime(date_text, "%Y-%m-%d")
+                # Mac/Linux: %-m, Windows: %#m（RenderはLinux）
+                current_date = date_obj.strftime("%-m/%-d")
+            except ValueError:
+                current_date = date_text
+
+        elif block["type"] == "paragraph":
             texts = block["paragraph"]["rich_text"]
             if texts:
-                content = "".join([t["plain_text"] for t in texts])
-                current_entry["html"] += f"<p class='diary-block'>{content}</p>\n"
+                full_text = "".join([t["plain_text"] for t in texts])
+                lines = full_text.split("\n")
+                for line in lines:
+                    if line.strip():
+                        current_lines.append(line.strip())
 
-    # 最後のエントリを追加
-    if current_entry:
-        diary.append(current_entry)
+    if current_date and current_lines:
+        html = "\n".join([f"<p class='diary-block'>{line}</p>" for line in current_lines])
+        diary.append({"date": current_date, "html": html})
 
     return diary
